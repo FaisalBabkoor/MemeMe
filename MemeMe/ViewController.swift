faa58b49839a6b3d6a0f2c30509e52a560e5bbdf
Faisal <faisal-f92@hotmail.com>
2019-10-04 04:15:07 +0300

    finish V1

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,166 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    //MARK:- IBOutlet
+    @IBOutlet var imagePickerView: UIImageView!
+    @IBOutlet var topTextField: UITextField!
+    @IBOutlet var bottomTextField: UITextField!
+    @IBOutlet var BottomToolBar: UIToolbar!
+    @IBOutlet var cameraButton: UIBarButtonItem!
+    @IBOutlet var sharButton: UIBarButtonItem!
+    @IBOutlet var topToolBar: UIToolbar!
+    
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: -3.0
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        styleTextField(topTextField)
+        styleTextField(bottomTextField)
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
+    func styleTextField(_ textField: UITextField){
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .center
+        textField.delegate = self
+    }
+    
+    
+    func pickImageFrom(_ source: UIImagePickerController.SourceType){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = source
+        present(imagePicker, animated: true)
+        
+        present(imagePicker, animated: true)
+    }
+    @IBAction func cameraButtonWasPressed(_ sender: Any) {
+        pickImageFrom(.camera)
+    }
+    @IBAction func albumcameraButtonWasPressed(_ sender: Any) {
+        pickImageFrom(.photoLibrary)
+    }
+    
+    
+    @objc func keyboardWillShow(_ notification: Notification){
+        if bottomTextField.isFirstResponder{
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification){
+        if bottomTextField.isFirstResponder{
+            view.frame.origin.y = 0.0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func unsubscribeFromKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        hideBar(true)
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show toolbar and navbar
+        hideBar(false)
+        return memedImage
+    }
+    
+    func hideBar(_ hide: Bool){
+        topToolBar.isHidden = hide
+        BottomToolBar.isHidden = hide
+    }
+    func save() {
+        // Create the meme
+        let memedImage = generateMemedImage()
+        _ = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imagePickerView.image!, memedImage: memedImage)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        picker.allowsEditing = true
+        if let image = info[.originalImage] as? UIImage{
+            imagePickerView.image = image
+            dismiss(animated: true, completion: nil)
+        }else if let cropImage = info[.editedImage] as? UIImage{
+            imagePickerView.image = cropImage
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM"{
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
 
-
+        return true
+    }
+    
+    @IBAction func shareButtonWasPressed(_ sender: Any) {
+        let memeImage = generateMemedImage()
+        let ac = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        ac.completionWithItemsHandler = {
+            activity, success, items, error in
+            if success{
+                self.save()
+            }
+        }
+        present(ac, animated: true)
+    }
+    
+ 
+    
 }
 
